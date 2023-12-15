c1570f2727ef19005ce277683327d783bc9a3bef
matheohero <134490234+matheohero@users.noreply.github.com>
2023-12-15 16:38:05 +0100

    Update BDD.sql

--- BDD.sql
+++ BDD.sql
@@ -1,6 +1,5 @@
-USE master;
-
 CREATE DATABASE IF NOT EXISTS game_collection;  
+USE game_collection;
 
 DROP TABLE IF EXISTS BILIOTEQUE;
 DROP TABLE IF EXISTS JEUX;
@@ -10,7 +9,7 @@ DROP TABLE IF EXISTS UTILISATEUR;
 CREATE TABLE UTILISATEUR(
    id_user VARCHAR(50),
    pren_user VARCHAR(50) NOT NULL,
-   mail_user VARCHAR(150) NOT NULL LIKE '%@%.%',
+   mail_user VARCHAR(150) CHECK (mail_user LIKE '%@%.%'),
    mdp_user VARCHAR(100) NOT NULL,
    nom_user VARCHAR(50) NOT NULL,
    PRIMARY KEY(id_user)
@@ -31,7 +30,7 @@ CREATE TABLE JEUX(
 CREATE TABLE BILIOTEQUE(
    id_user VARCHAR(50),
    id_jeux VARCHAR(50),
-   temp_jeux VARCHAR(50) WHERE temp_jeux >= 0 NOT NULL,
+   temp_jeux INT CHECK (temp_jeux >= 0),
    PRIMARY KEY(id_user, id_jeux),
    FOREIGN KEY(id_user) REFERENCES UTILISATEUR(id_user),
    FOREIGN KEY(id_jeux) REFERENCES JEUX(id_jeux)
