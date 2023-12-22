3248fe76b9308b68ce5e8f95be4b77587c01e10f
Touflapi <148531077+Touflapi@users.noreply.github.com>
2023-12-22 10:06:52 +0100

    controller auth -  createAccount

--- controllers/inscription.php
+++ controllers/inscription.php
@@ -1,6 +1,11 @@
 <?php
 
 require './views/inscription.php';
+
+if (isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["password"])&& isset($_POST["confirmed-password"])){
+    createAccount($_POST["name"],$_POST["surname"],$_POST["password"],$_POST["confirmed-password"]);
+}
+
 require './views/footer.php';
 
 ?>
