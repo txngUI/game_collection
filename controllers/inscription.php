c25ddb06fe9612f8f8b0e98a55c64f77eeaadfca
Touflapi <148531077+Touflapi@users.noreply.github.com>
2023-12-22 10:20:50 +0100

    Update Controller inscription

--- controllers/inscription.php
+++ controllers/inscription.php
@@ -2,8 +2,8 @@
 
 require './views/inscription.php';
 
-if (isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["password"])&& isset($_POST["confirmed-password"])){
-    createAccount($_POST["name"],$_POST["surname"],$_POST["password"],$_POST["confirmed-password"]);
+if (isset($_POST["name"]) && isset($_POST["surname"]) && isset($_POST["password"])&& isset($_POST["email"])&&isset($_POST["confirmed-password"]) && isset($_POST["confirmed-password"])){
+    createAccount($_POST["name"],$_POST["surname"],$_POST["password"],$_POST["confirmed-password"],$_POST["email"]);
 }
 
 require './views/footer.php';
