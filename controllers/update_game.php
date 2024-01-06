8bffad8a9228c6a06fe5d56164543e7801a2ec53
matheohero <matheobideault@gmail.com>
2024-01-06 14:35:30 +0100

    dev+lien page update

--- controllers/update_game.php
+++ controllers/update_game.php
@@ -1,16 +1,33 @@
 <?php
 
 require './Models/jeu.php';
-
+session_start();
 $valueError = 0;
 
+if (isset($_GET['IdGame'])) {
+  $game = getGameWithIdAndUser($_GET['IdGame'],$_SESSION['id_user']);
+
+
 if(isset($_POST["time-played"])){
+  $time = (int)($_POST["time-played"]); // conversion en int
+
   //fonction qui modifie le temps de jeu et return 1 si il y a une erreur
-  $valueError = changePlayTime($_POST["time-played"]); //(Recupération du nom du jeu par la méthode get lors de la redirection [la redirection n'est pas là])
+  $valueError = updateTimeGame($_GET['IdGame'],$_SESSION['id_user'],$time); //(Recupération du nom du jeu par la méthode get lors de la redirection [la redirection n'est pas là])
+  if (!$valueError) {
+    header("Location: index.php?action=home");
+  }
 }
 
 //Pour la suppression du jeu de la bibliotheque
-//delGameLibrary();
+if (isset($_POST['delete-game'])) {
+  deleteGameFromLibrary($_GET['IdGame'],$_SESSION['id_user']);
+  header("Location: index.php?action=home");
+}
+
+}
+
+
+
 
 require './views/header.php';
 require './views/update_game.php';
--- views/update_game.php
+++ views/update_game.php
@@ -9,9 +9,9 @@
 <body>
   <div class="game_update">
     <div class="desc">
-      <h1>Read Dead Redemption</h1>
-      <p class="description">Jeu trop cool</p>
-      <p class="time-played">Temps passé : 60h</p>
+      <h1><?php echo $game['nom_jeux']?></h1>
+      <p class="description"><?php echo $game['desc_jeux']?></p>
+      <p class="time-played">Temps passé : <?php echo $game['temp_jeux']?>  h</p>
       <h2>Modifier le temps passé sur le jeu</h2>
       <form method="post">
       <div class="modify-time">
@@ -19,15 +19,15 @@
         <input type="number" name="time-played" id="time-played"/>
       </div>
       <div class="submit" style="margin-top: 15px;">
-        <input type="submit" class="add-game"value="AJOUTER">
+        <input type="submit" class="add-game" value="AJOUTER">
       </div>
       <div class="submit" style="margin-top: 15px;">
-        <input type="submit" class="delete-game"value="SUPPRIMER LE JEU DE MA BIBLIOTHEQUE">
+        <input type="submit" name="delete-game" class="delete-game" value="SUPPRIMER LE JEU DE MA BIBLIOTHEQUE">
       </div>
       </form>
     </div>
     <div class="img">
-      <img src="https://prod-printler-front-as.azurewebsites.net/media/photo/148332-1.jpg?mode=crop&width=727&height=1024&rnd=0.0.1" style="width:200px;" alt="Red Dead Redemption">
+      <img src="<?php echo $game['img_jeux']?>" style="width:200px;" alt="Red Dead Redemption">
     </div>
 
   </div>
